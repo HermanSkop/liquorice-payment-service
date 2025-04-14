b695ef9795f0ba028f0ba19142cf7600e62347d7
HermanSkop <armaton111@gmail.com>
2025-04-14 23:29:41 +0200

    Unit test

--- src/test/java/org/example/liquoricepaymentservice/LiquoricePaymentServiceApplicationTests.java
+++ src/test/java/org/example/liquoricepaymentservice/LiquoricePaymentServiceApplicationTests.java
@@ -2,8 +2,10 @@ package org.example.liquoricepaymentservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class LiquoricePaymentServiceApplicationTests {
 
     @Test
